a7060a32425a71faa7043b937d61320bbac1e716
Ahmad Sawalha <122925762+Abash05@users.noreply.github.com>
2026-04-29 14:59:16 -0700

    Add files via upload

---
+++ Shark-Tank-Notebook.py
@@ -0,0 +1,10 @@
+# imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load the data
+data = pd.read_csv('shark_tank_companies.csv')
+
+# display the first few rows of the data
+print(data.head())
